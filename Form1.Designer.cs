5d7dc367c9869adc3a3d5f147aa5668e4e1abd3a
Minord <benderrobot12@gmail.com>
2019-10-25 17:27:11 -0600

    add basic files

---
+++ Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.productName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precioUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descuento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IVA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxPrecioUnitario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxCantidad = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productName,
+            this.precioUnitario,
+            this.cantidad,
+            this.descuento,
+            this.IVA,
+            this.total});
+            this.dataGridView.Location = new System.Drawing.Point(12, 211);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(644, 227);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // productName
+            // 
+            this.productName.HeaderText = "Nombre";
+            this.productName.Name = "productName";
+            // 
+            // precioUnitario
+            // 
+            this.precioUnitario.HeaderText = "Precio Unitario";
+            this.precioUnitario.Name = "precioUnitario";
+            // 
+            // cantidad
+            // 
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.Name = "cantidad";
+            // 
+            // descuento
+            // 
+            this.descuento.HeaderText = "Descuento";
+            this.descuento.Name = "descuento";
+            // 
+            // IVA
+            // 
+            this.IVA.HeaderText = "13% IVA";
+            this.IVA.Name = "IVA";
+            // 
+            // total
+            // 
+            this.total.HeaderText = "Total";
+            this.total.Name = "total";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxCantidad);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxPrecioUnitario);
+            this.groupBox1.Controls.Add(this.textBoxNombre);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(333, 159);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Producto";
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(111, 26);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(128, 20);
+            this.textBoxNombre.TabIndex = 0;
+            // 
+            // textBoxPrecioUnitario
+            // 
+            this.textBoxPrecioUnitario.Location = new System.Drawing.Point(111, 53);
+            this.textBoxPrecioUnitario.Name = "textBoxPrecioUnitario";
+            this.textBoxPrecioUnitario.Size = new System.Drawing.Size(128, 20);
+            this.textBoxPrecioUnitario.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Precio Unitario:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(136, 120);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(103, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Añadir producto";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(364, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 159);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Informacion de descuento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Rango de Venta |  Porcentaje";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "De $100.01 hasta $200.00 ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "De $200.01 hasta $300.00";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "De $300.01 hasta $500.00 ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Más de $500.00 ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(180, 56);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "7%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(180, 81);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "11%";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(180, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "17%";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(180, 130);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 13);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "25%";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(17, 81);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(52, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Cantidad:";
+            // 
+            // textBoxCantidad
+            // 
+            this.textBoxCantidad.Location = new System.Drawing.Point(111, 81);
+            this.textBoxCantidad.Name = "textBoxCantidad";
+            this.textBoxCantidad.Size = new System.Drawing.Size(128, 20);
+            this.textBoxCantidad.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioUnitario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descuento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IVA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn total;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPrecioUnitario;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxCantidad;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool validarNombre() {
+            if (textBoxNombre.Text == "")
+            {
+                //campo vacio 
+                MessageBox.Show("El campo nombre no puede estar vacio");
+                return false;
+            }
+            else {
+                //es valido
+                return true;
+            }
+            return false;
+        }
+
+        private float validarPrecioUnitario() {
+            float output;
+            if (float.TryParse(textBoxPrecioUnitario.Text, out output))
+            {
+                if (output >= 0)
+                {
+                    return output;
+                }
+                else {
+                    MessageBox.Show("El campo Precio Unitario tiene que ser positivo");
+                    return -1;
+                }
+            }
+            else {
+                MessageBox.Show("Inserte un numero entero o decimal en el campo Precio Unitario");
+                return -1;
+            }
+            return -1;
+        }
+
+        private int validarCantidad() {
+            int output;
+            if (Int32.TryParse(textBoxCantidad.Text, out output))
+            {
+                if (output >= 0)
+                {
+                    return output;
+                }
+                else
+                {
+                    MessageBox.Show("El campo Cantidad tiene que ser positivo");
+                    return -1;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Solo es valido un numero entero en el campo Cantidad");
+                return -1;
+            }
+            return -1;
+        }
+
+        private float calcularDescuento(float total) {
+            if (total >= 500) {
+                return total * 0.25f;
+            }
+            else if (total >= 300)
+            {
+                return total * 0.17f;
+            }
+            else if (total >= 200)
+            {
+                return total * 0.11f;
+            }
+            else if (total >= 100)
+            {
+                return total * 0.07f;
+            }
+            return 0;
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            //declaracion de variables
+            bool entradasValidas = true;
+
+            string Nombre = "";
+            float PrecioUnitario = 0.0f;
+            int Cantidad = 0;
+
+            //validar nombre
+            if (validarNombre())
+            {
+                Nombre = textBoxNombre.Text;
+            }
+            else {
+                entradasValidas = false;
+            }
+
+            //validar precio unitario
+            PrecioUnitario = validarPrecioUnitario();
+            if (PrecioUnitario == -1)
+            {
+                entradasValidas = false;
+            }
+
+            //validar cantidad
+            Cantidad = validarCantidad();
+            if (Cantidad == -1)
+            {
+                entradasValidas = false;
+            }
+
+
+            if (entradasValidas) {
+                MessageBox.Show("Validacion exitosa");
+
+                //calcular totales y descuentos
+                float subTotal = PrecioUnitario * Cantidad;
+                float descuento = calcularDescuento(subTotal);
+
+                float total = subTotal - descuento;
+                float totalMasIVA = total * 1.13f;
+
+                //arreglo del producto
+                string[] product_info = new string[6];
+                int nameIndex = 0;
+                int priceIndex = 1;
+                int amountIndex = 2;
+                int discountIndex = 3;
+                int IVAIndex = 4;
+                int TotalIndex = 5;
+
+                //poner la informacion en el array
+                product_info[nameIndex] = Nombre;
+                product_info[priceIndex] = PrecioUnitario.ToString("C");
+                product_info[amountIndex] = Cantidad.ToString();
+                product_info[discountIndex] = descuento.ToString("C");
+                product_info[IVAIndex] = (total * 0.13f).ToString("C");
+                product_info[TotalIndex] = totalMasIVA.ToString("C");
+
+                //precentarlo en GridBox
+                dataGridView.Rows.Add();
+                
+
+                //limpiar cajas de texto
+                textBoxNombre.Text = "";
+                textBoxPrecioUnitario.Text = "";
+                textBoxCantidad.Text = "";
+            }
+        }
+    }
+}
